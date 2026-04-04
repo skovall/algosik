11af8e2296979cf7f1e3e03e8bce263aeeb5fe00
skovall <stetskovall@gmail.com>
2026-04-04 20:03:34 +0300

    Update code.py

--- lab-2/code.py
+++ lab-2/code.py
@@ -1,3 +1,6 @@
+import base64
+import json
+
 class Bus_Stop:
     def __init__(self, name, coordinates, time_to_next):
         self.name = name
@@ -13,3 +16,17 @@ class Bus_Stop:
                 "coordinates": self.coordinates,
                 "time_to_next": self.time_to_next
             }
+    
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data["name"], data["coordinates"], data["time_to_next"])
+    
+    def to_base64(self):
+        json_str = json.dumps(self.to_dict())
+        return base64.b64encode(json_str.encode()).decode()
+    
+    @classmethod
+    def from_base64(cls, base64_str):
+        json_str = base64.b64decode(base64_str).decode()
+        data = json.loads(json_str)
+        return cls.from_dict(data)
