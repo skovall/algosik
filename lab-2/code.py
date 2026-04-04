8ca3a1326cb91d36d00ca2895180b79fe8bd7512
skovall <stetskovall@gmail.com>
2026-04-04 13:30:16 +0300

    Update code.py

--- lab-2/code.py
+++ lab-2/code.py
@@ -1,1 +1,15 @@
+class Bus_Stop:
+    def __init__(self, name, coordinates, time_to_next):
+        self.name = name
+        self.coordinates = coordinates
+        self.time = time_to_next
 
+    def show_info(self):
+        return f"Остановка {self.name}\nКоординаты: {self.coordinates}\nВремя до следующей: {self.time}"
+
+    def to_dict(self):
+            return {
+                "name": self.name,
+                "coordinates": self.coordinates,
+                "time_to_next": self.time_to_next
+            }
