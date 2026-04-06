75d2286ac53944772a986e4bbdeb6d180e068a76
skovall <stetskovall@gmail.com>
2026-04-06 13:26:04 +0300

    Update code.py

--- lab-2/code.py
+++ lab-2/code.py
@@ -5,7 +5,7 @@ class Bus_Stop:
     def __init__(self, name, coordinates, time_to_next):
         self.name = name
         self.coordinates = coordinates
-        self.time = time_to_next
+        self.time_to_next = time_to_next
 
     def show_info(self):
         return f"Остановка {self.name}\nКоординаты: {self.coordinates}\nВремя до следующей: {self.time}"
@@ -30,3 +30,31 @@ class Bus_Stop:
         json_str = base64.b64decode(base64_str).decode()
         data = json.loads(json_str)
         return cls.from_dict(data)
+
+class Bus_route:
+    def __init__(self):
+        self.stops = []
+
+    def add_stop(self, stop):
+        self.stops.append(stop)
+        print(f"В маршрут добавлена остановка {stop.name}.")
+
+    def remove_stop(self, name):
+        for stop in self.stops:
+            if stop.name == name:
+                self.stops.remove(stop)
+                print(f"Остановка '{name}' удалена из маршрута")
+                return True
+        print(f"Остановка '{name}' не найдена")
+        return False
+
+    def calculate_full_time(self):
+        if not self.stops:
+            return 0
+        full_time = 0
+        for stop in self.stops:
+            full_time += stop.time_to_next
+        return full_time
+route = Bus_route()
+stop = Bus_Stop('jijo','898090','90')
+route.add_stop(stop)
